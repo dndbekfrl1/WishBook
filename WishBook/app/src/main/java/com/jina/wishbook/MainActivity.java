7d372e24c9de39c550bf135435f818a94d01cf10
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-05-05 01:26:18 +0900

    초기

--- WishBook/app/src/main/java/com/jina/wishbook/MainActivity.java
+++ WishBook/app/src/main/java/com/jina/wishbook/MainActivity.java
@@ -1,14 +1,51 @@
 package com.jina.wishbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private CameraFragment cameraFragment = new CameraFragment();
+    private WishFragment wishFragment = new WishFragment();
+    private CalendarFragment calendarFragment = new CalendarFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.flagment_container,calendarFragment).commitAllowingStateLoss();
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        switch (item.getItemId()) {
+                            case R.id.camera:
+                                fragmentTransaction.replace(R.id.flagment_container, cameraFragment).commit();
+                                return true;
+                            case R.id.wish:
+                                fragmentTransaction.replace(R.id.flagment_container, wishFragment).commit();
+                                return true;
+                            case R.id.home:
+                                fragmentTransaction.replace(R.id.flagment_container,calendarFragment).commit();
+                        }
+                        return false;
+                    }
+
+                });
+
     }
 }
